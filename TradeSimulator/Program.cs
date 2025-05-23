628890628bf9faf591efa5d68c59c0a6bdf0e10b
Victor <victor.danneels@epitech.eu>
2025-05-23 14:43:24 +0900

    Implemented simple chat app for learning purpose

--- TradeSimulator/Program.cs
+++ TradeSimulator/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.ResponseCompression;
 using TradeSimulator.BlazorClient.Pages;
 using TradeSimulator.Components;
+using TradeSimulator.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
+// SignalR
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        ["application/octet-stream"]);
+});
+
+
+
+/* ----------------------------------------------------- */
+
 var app = builder.Build();
+
+// Note from Microsoft doc: When using Hot Reload, disable Response Compression Middleware in the Development environment.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,5 +51,7 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(TradeSimulator.BlazorClient._Imports).Assembly);
+
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
