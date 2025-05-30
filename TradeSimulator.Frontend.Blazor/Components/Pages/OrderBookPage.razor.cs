70c36313a5439da594163c70010be2d9d39a5a8e
Victor <victor.danneels@epitech.eu>
2025-05-30 12:08:08 +0900

    Cleaned up code in OrderBook page

--- TradeSimulator.Frontend.Blazor/Components/Pages/OrderBookPage.razor.cs
+++ TradeSimulator.Frontend.Blazor/Components/Pages/OrderBookPage.razor.cs
@@ -14,26 +14,12 @@ namespace TradeSimulator.Frontend.Blazor.Components.Pages
 {
     public class OrderBookPageBase : ComponentBase
     {
-
-        private readonly string _defaultHubURL = "http://localhost:5038/trade-hub";
-
         [Parameter]
         public string TickerId { get; set; }
 
         [Inject] NavigationManager Navigation { get; set; }
-        [Inject] ISnackbar Snackbar { get; set; }
-        [Inject] IDialogService DialogService { get; set; }
         [Inject] TradeService TradeService { get; set; }
 
-        protected string BrokerId;
-        protected List<Ticker> Tickers;
-
-        private List<string> messages = [];
-        private string userInput;
-        private string messageInput;
-
-        private bool _isConnecting = false;
-        private bool _isDisconnecting = false;
 
         protected Ticker _ticker;
         protected bool _isLoading = false;
@@ -51,94 +37,6 @@ namespace TradeSimulator.Frontend.Blazor.Components.Pages
             _ticker = await TradeService.GetTickerById(TickerId);
 
             _isLoading = false;
-        }
-
-        protected async Task ConnectAndGetTickers()
-        {
-            await Connect();
-            await GetTickers();
-        }
-
-        protected async Task Connect()
-        {
-            Console.WriteLine("Connecting...");
-
-            _isConnecting = true;
-
-            try
-            {
-                await TradeService.Connect(_defaultHubURL, BrokerId);
-
-                Console.WriteLine("Connected !");
-                Snackbar.Add($"Successfully Connected to hub", Severity.Success);
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("Failed to connect.");
-                Snackbar.Add($"Failed to connect. {exception.Message}", Severity.Error);
-            }
-            finally
-            {
-                _isConnecting = false;
-            }
-        }
-
-        protected async Task Disconnect()
-        {
-            Console.WriteLine("Disconnecting...");
-
-            _isDisconnecting = true;
-
-            try
-            {
-                await TradeService.Disconnect();
-
-                Console.WriteLine("Disconnected.");
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("Failed to disconnect.");
-                Snackbar.Add($"Failed to disconnect. {exception.Message}", Severity.Error);
-            }
-            finally
-            {
-                _isDisconnecting = false;
-            }
-        }
-
-        protected async Task GetTickers()
-        {
-            Tickers = await TradeService.GetTickers();
-
-            foreach (var ticker in Tickers)
-                Console.WriteLine($"[{ticker.Id}] {ticker.DisplayName} ({ticker.Orders.Count})");
-        }
-
-        protected async Task OpenOrderBook()
-        {
-            var parameters = new DialogParameters<CreateOrderBookDialog>
-            {
-                { x => x.Tickers, Tickers },
-            };
-
-            var dialog = await DialogService.ShowAsync<CreateOrderBookDialog>(null, parameters);
-            var result = await dialog.Result;
-
-            if (!result.Canceled)
-            {
-                Ticker ticker = result.Data as Ticker;
-
-                Console.WriteLine(ticker.DisplayName);
-            }
-        }
-
-        public bool IsConnected => TradeService.IsConnected;
-        public bool IsConnecting => _isConnecting;
-        public bool IsDisconnecting => _isDisconnecting;
-
-        private string GenerateRandomBrokerId()
-        {
-            return "Victor";
         }
     }
 }
