afb0b630d49f4d268ba524699b5a612ae5baec38
Victor <victor.danneels@epitech.eu>
2025-05-30 10:36:32 +0900

    Moved IEquatable interface from Ticker to EntityBase to extend this code to all entities.

--- TradeSimulator.Shared/Models/EntityBase.cs
+++ TradeSimulator.Shared/Models/EntityBase.cs
@@ -1,17 +1,41 @@
 ﻿namespace TradeSimulator.Shared.Models
 {
-    public abstract class EntityBase : IEntity<string>
+    public abstract class EntityBase : IEntity<string>, IEquatable<EntityBase>
     {
         public string Id { get; set; }
+
+
+
+        /* ---------------------------------------------------- */
 
         public EntityBase()
         {
             Id = GenerateId();
         }
 
+
+
+        /* ---------------------------------------------------- */
+
         protected virtual string GenerateId()
         {
             return Guid.NewGuid().ToString().Substring(0, 8);
         }
+
+
+
+        /* ---------------------------------------------------- */
+
+        public bool Equals(EntityBase other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj) => obj is EntityBase state && Equals(state);
+
+        public override int GetHashCode() => Id.GetHashCode();
     }
 }
--- TradeSimulator.Shared/Models/Ticker.cs
+++ TradeSimulator.Shared/Models/Ticker.cs
@@ -2,26 +2,10 @@
 
 namespace TradeSimulator.Shared.Models
 {
-    public class Ticker : EntityBase, IEquatable<Ticker>
+    public class Ticker : EntityBase
     {
         public string DisplayName { get; set; }
 
         public List<Order> Orders { get; set; }
-
-        public bool Equals(Ticker other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-
-            return Id == other.Id;
-        }
-
-        public override bool Equals(object obj) => obj is Ticker state && Equals(state);
-
-        public override int GetHashCode() => Id.GetHashCode();
-
-        // Also important to override ToString, otherwise the object will be displayed as the full class name
-        // Alternatively you can use ToStringFunc="x => x.Name" in MudSelect
-        public override string ToString() => DisplayName;
     }
 }
