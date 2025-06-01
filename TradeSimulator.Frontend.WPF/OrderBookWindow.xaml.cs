a07d4ebc5d30086dcfa090de34b7cc0fe11a6859
Victor <victor.danneels@epitech.eu>
2025-06-01 11:34:56 +0900

    Added order book window

---
+++ TradeSimulator.Frontend.WPF/OrderBookWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TradeSimulator.Shared.Models;
+using TradeSimulator.Shared.Services;
+
+namespace TradeSimulator.Frontend.WPF
+{
+    /// <summary>
+    /// Interaction logic for OrderBookWindow.xaml
+    /// </summary>
+    public partial class OrderBookWindow : Window
+    {
+        private TradeService TradeService;
+        private Ticker Ticker;
+
+        public OrderBookWindow(TradeService tradeService, Ticker ticker)
+        {
+            InitializeComponent();
+
+            TradeService = tradeService;
+            Ticker = ticker;
+
+            OrdersListView.ItemsSource = Ticker.Orders;
+            TickerTextBox.Text = $"Ticker: {Ticker.DisplayName}";
+        }
+    }
+}
