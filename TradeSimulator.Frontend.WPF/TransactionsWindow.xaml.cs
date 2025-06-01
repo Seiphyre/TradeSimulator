c1654ad29e9a2b2ec0d840d356cb67b4e7d31161
Victor <victor.danneels@epitech.eu>
2025-06-01 11:15:31 +0900

    Added Transaction window

---
+++ TradeSimulator.Frontend.WPF/TransactionsWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TradeSimulator.Shared.Models;
+using TradeSimulator.Shared.Services;
+
+namespace TradeSimulator.Frontend.WPF
+{
+    /// <summary>
+    /// Interaction logic for TransactionsWindow.xaml
+    /// </summary>
+    public partial class TransactionsWindow : Window
+    {
+        private TradeService TradeService;
+        private List<Transaction> Transactions;
+
+        public TransactionsWindow(TradeService tradeService, List<Transaction> transactions)
+        {
+            InitializeComponent();
+
+            TradeService = tradeService;
+            Transactions = transactions;
+
+            TransactionsListView.ItemsSource = Transactions;
+        }
+    }
+}
